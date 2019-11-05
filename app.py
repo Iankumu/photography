73e3effd959ee224a93335e50c36a5fa798e200f
Daniel Waruo <warudaniel@gmail.com>
2019-11-05 15:26:55 +0300

    implement flask security and sql alchemy

--- app.py
+++ app.py
@@ -1,222 +1,53 @@
 import os
-import uuid
-from flask_change_password import flask_change_password, ChangePasswordForm, ChangePassword, SetPasswordForm
-from flask_security import reset_password_instructions_sent, password_reset
+from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_mail import Mail
-from comparison import image_comparison
-import methods
-from functools import wraps
-from flask import Flask, render_template, flash, redirect, url_for, session, request
-from flask_mysqldb import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_security import Security, login_required, SQLAlchemySessionUserDatastore
 from werkzeug.utils import secure_filename
-from wtforms import Form, PasswordField, validators, StringField, BooleanField, SubmitField
-from wtforms.validators import InputRequired, ValidationError
+from comparison import image_comparison
+from config import Config
+from database import db_session
+from forms import RegistrationForm
+from models import User, Role
 
-UPLOAD_FOLDER = 'C:/Users/User/Documents/Strathmore/ICS/Academic Work/Year 2/Semester 2/IS Project/photography/static'
-CLIENT_FOLDER = 'C:/Users/User/Documents/Strathmore/ICS/Academic Work/Year 2/Semester 2/IS Project/photography/static/Client_Uploads'
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret123'
+# set up the configurations
+app.config.from_object(Config)
+# Setup Flask-Security
+user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
+security = Security(app, user_datastore, register_form=RegistrationForm)
 
-# config MYSQL
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'photography'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
-# initialize MYSQL
-mysql = MySQL(app)
-
-# smtp
-app.config['MAIL_SERVER'] = 'stmp.goolemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-
-mail = Mail(app)
-
-# config image format
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['CLIENT_FOLDER'] = CLIENT_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
-
-app.config['SECURITY_RECOVERABLE'] = True
+mail = Mail()
+mail.init_app(app)
 
 
+# renders the home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
+# renders the about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 
-# Registration Form Class
-class RegistrationForm(Form):
-    name = StringField('FullName', [validators.length(min=1, max=50)])
-    username = StringField('UserName', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=35)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Password does not match')
-    ])
-    confirm = PasswordField('Confirm Password')
-
-
-# Login Form
-class LoginForm(Form):
-    emailLogin = StringField('Email', [validators.length(min=6, max=50)], validators=[InputRequired()])
-    passwordLogin = PasswordField(validators.length(min=8, max=50), validators=[InputRequired()])
-    remember = BooleanField('Remember Me')
-
-
-# Update Form
-class UpdateForm(Form):
-    name = StringField('FullName')
-    username = StringField('UserName')
-    email = StringField('Email')
-
-
-class RequestResetForm(Form):
-    email = StringField('Email', [validators.Length(min=6, max=35)])
-    submit = SubmitField('Request Password Reset')
-
-    @staticmethod
-    def validate_email(email):
-        cur = mysql.connection.cursor()
-        cur.execute('SELECT Email FROM users')
-        id = cur.fetchall()
-        for row in id:
-            user = row['Email']
-            if user == email:
-                raise ValidationError('There is no account with that Email.. You must Register First')
-
-
-class PasswordResetForm(Form):
-    password = PasswordField('Password', [validators.DataRequired(),
-                                          validators.EqualTo('confirm', message='Password does not match')])
-    confirmPassword = PasswordField('Confirm Password')
-    submit = SubmitField('Reset Password')
-
-
-# Registration
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        name = form.name.data
-        email = form.email.data
-        username = form.username.data
-        # encrypting password
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-
-        # Creating Cursor
-
-        cur = mysql.connection.cursor()
-        # Execute Query
-        cur.execute("INSERT INTO users(FullName, UserName, Email, Password) VALUES (%s, %s, %s, %s)",
-                    (name, username, email, hashed_password))
-
-        # Commit to DB
-        mysql.connection.commit()
-
-        # Close Connection
-        cur.close()
-
-        flash('You Are Registered Successfully and you can now log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-
-
-# User Login
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-
-        # Getting form fields
-        email = request.form['email']
-        password_got = request.form['password']
-
-        # Create Cursor
-        cur = mysql.connection.cursor()
-
-        # Get Email and Password
-        result = cur.execute("SELECT * FROM users WHERE Email = %s", [email])
-        if result > 0:
-            # Get stored hash password
-            data = cur.fetchone()
-            password = data['Password']
-
-            # Compare Passwords
-            if check_password_hash(password, password_got):
-                # Password matched
-                session['Logged_in'] = True
-                session['name'] = data['FullName']
-                session['id'] = data['UserID']
-                flash('Login Successful', 'success')
-                return redirect(url_for('dashboard'))
-
-            else:
-                error = 'Invalid Login'
-            return render_template('login.html', error=error)
-            # Close Connection
-        else:
-            error = 'User Not Found'
-            return render_template('login.html', error=error)
-    return render_template('login.html')
-
-
-# Check if user is logged in
-def is_logged_in(verify):
-    @wraps(verify)
-    def wrap(*args, **kwargs):
-        if 'Logged_in' in session:
-            return verify(*args, **kwargs)
-        else:
-            flash('Unauthorized, Please Login', 'danger')
-            return redirect(url_for('login'))
-
-    return wrap
+# renders the users profile page
+@app.route('/profile', methods=['GET'])
+def profile():
+    # get the user
+    # get a user details
+    # render a profile page of the user
+    return render_template('profile.html')
 
 
 # Dashboard
+@login_required
 @app.route('/dashboard')
-@is_logged_in
 def dashboard():
-    # Fetching the image paths from mysql
-    if login:
-        user = session['id']
-        cur = mysql.connection.cursor()
-        result = cur.execute("SELECT * FROM photos WHERE photographerid=%s", [user])
-
-        rows = cur.fetchall()
-        temp = []
-        for row in rows:
-            data = row['photo']
-            date = row['date_posted']
-            temp.append(data)
-        mysql.connection.commit()
-        cur.close()
-        if not result:
-            return render_template('dashboard.html')
-
+    temp = []
+    date = "20-4-2000"
     return render_template('dashboard.html', image_names=temp, date=date)
-
-
-# Logout
-@app.route('/logout')
-def logout():
-    session.clear()
-    flash('You are now Logged out', 'success')
-    return redirect(url_for('home'))
 
 
 # uploading files
@@ -235,15 +66,10 @@ def allowed_file(filename):
         return False
 
 
-# renders the upload html page
-@app.route('/uploads')
-def upload_form():
-    return render_template('uploads.html')
-
-
-@app.route('/uploads', methods=['POST', 'GET'])
-@is_logged_in
-def upload_file():
+@login_required
+@app.route('/photographer/uploads', methods=['POST', 'GET'])
+def photographer_upload():
+    # if request method is post
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -261,20 +87,19 @@ def upload_file():
             flash("Image Uploaded Successfully", "success")
 
             # inserting file path to the database
-            if login:
-                user = session['id']
-                cur = mysql.connection.cursor()
-                cur.execute("INSERT INTO photos (photo,photographerid)VALUES (%s,%s)", [file_name, user])
-                mysql.connection.commit()
-                cur.close()
-                return redirect(url_for('dashboard'))
+            # save the photograph
+            flash("Image Uploaded Successfully", "success")
+            return redirect(url_for('dashboard'))
 
         else:
             flash('Allowed file types are .png, .jpg, .jpeg')
             return redirect(request.url)
+    # if the request is any other than get
+    return render_template('uploads.html')
 
 
-@app.route('/client_uploads', methods=['POST', 'GET'])
+@login_required
+@app.route('/upload', methods=['POST', 'GET'])
 def client_upload():
     if request.method == 'POST':
         # check if the post request has any files
@@ -295,12 +120,9 @@ def client_upload():
             file_path = os.path.join(app.config['CLIENT_FOLDER'], filename)
             # save file
             file.save(file_path)
-            # initiate cursor
-            cur = mysql.connection.cursor()
-            # get all photos
-            cur.execute("SELECT * FROM photos")
+
             # get all photos after ranking then
-            photos = cur.fetchall()
+            photos = [object]
             ranked_photos = image_comparison(file_path, photos)
             # map the photos with the photographer and return the photographer name ranked in order of average
             # return list of photographer objects structure {name,link )
@@ -315,107 +137,16 @@ def client_upload():
         return render_template('Comparison.html')
 
 
-@app.route('/edit', methods=['POST', 'GET'])
-@is_logged_in
-def edit():
-    if login:
-        userid = session['id']
-        cur = mysql.connection.cursor()
-        cur.execute('SELECT * FROM photos WHERE photographerid = %s', [userid])
-        id = cur.fetchall()
-        photos = []
-        photoid = []
-        for row in id:
-            data = row['photoid']
-            name = row['photo']
-            photoid.append(data)
-            photos.append(name)
-        mysql.connection.commit()
-        cur.close()
-        allImages = dict(zip(photoid, photos))
-        print(allImages)
-
-        # os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-    return redirect('dashboard')
-
-
-@app.route('/Reset_Password', methods=['GET', 'POST'])
-def reset_request():
-    form = RequestResetForm()
-    if not is_logged_in(verify=True):
-        form = RequestResetForm()
-        if form.validate():
-            email = request.form['Email']
-            cur = mysql.connection.cursor()
-            cur.execute("SELECT * From Users WHERE email =%s", [email])
-            data = cur.fetchone()
-            userid = data['UserID']
-            user = methods.get_reset_token(userid=userid)
-            print(email)
-            methods.send_reset_email(user)
-            flash('An Email has been sent with instructions to reset the password', 'info')
-            return redirect(url_for('login'))
-    return render_template('reset_request.html', title='Reset Password', form=form)
-
-
-@app.route('/Reset_Password/<token>', methods=['GET', 'POST'])
-def reset_token(token):
-    if is_logged_in(verify=True):
-        return redirect(url_for('dashboard'))
-
-    if not is_logged_in():
-        user = methods.verify(token)
-        if user is None:
-            flash('That is an invalid or expired token ', 'warning')
-        return redirect(url_for('reset_request '))
-    form = PasswordResetForm()
-    if form.validate():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        cur = mysql.connection.cursor()
-        cur.execute("UPDATE users SET Password = %s", [hashed_password])
-        mysql.connection.commit()
-        cur.close()
-        flash('Your passsword has been updated', 'success')
-        return redirect(url_for('login'))
-    return render_template('reset_token.html', title='Reset Password', form=form)
-
-
-@app.route('/profile', methods=['POST', 'GET'])
-def profile():
-    form = UpdateForm(request.form)
-    if form.validate():
-        username = form.username.data
-        Fullname = form.name.data
-        email = form.email.data
-    if request.method == 'GET':
-        if login:
-            user = session['id']
-            cur = mysql.connection.cursor()
-            cur.execute('SELECT * FROM users WHERE UserID=%s', [user])
-            rows = cur.fetchall()
-            for row in rows:
-                uname = row['UserName']
-                name = row['FullName']
-                mail = row['Email']
-
-                form.username.data = uname
-                form.email.data = mail
-                form.name.data = name
-            mysql.connection.commit()
-            cur.close()
-    elif request.method == 'POST':
-        if login:
-            user = session['id']
-            cur = mysql.connection.cursor()
-            cur.execute('UPDATE users SET UserName=%s, FullName = %s, Email = %s WHERE UserID = %s',
-                        [username, Fullname, email, user])
-            mysql.connection.commit()
-            cur.close()
-        flash('Your account has been updated!', 'success')
-
-        return redirect(url_for('profile'))
-    return render_template('profile.html', form=form)
+# as a photographer edit a photo
+@login_required
+@app.route('/edit/<photo_id>', methods=['POST', 'GET'])
+def edit(photo_id):
+    if request.method == "GET":
+        # show the edit photo page
+        return redirect('dashboard')
+    if request.method == "POST":
+        # save the edited images
+        pass
 
 
 # Server Startup
--- comparison.py
+++ comparison.py
@@ -1,39 +1,7 @@
-import glob
 import numpy as np
 from PIL import Image
 from sklearn.metrics.pairwise import euclidean_distances
 import os
-
-images = glob.glob(os.getcwd() + "/static**/*jpg", recursive=True)
-
-
-def test_euclidean():
-    photographer_images = images
-    my_image = os.path.join(os.getcwd(), "static/ampersand-creative-co-pp_oXEb2H48-unsplash.jpg")
-    my_image = Image.open(my_image)
-    my_image = my_image.resize((400, 400))
-    my_image = np.array(my_image)
-    my_image = my_image.flatten()
-    compared_images = []
-
-    for image in photographer_images:
-        # open the image
-        image = Image.open(image)
-        # resize the image
-        image = image.resize((400, 400))
-        # turn image into numpy nd array
-        image = np.array(image)
-        # flatten the image
-        image = image.flatten()
-        # get the euclidean distance
-        difference = euclidean_distances([image], [my_image])
-        print(difference[0][0])
-        compared_images.append(
-            (3, difference[0][0])
-        )
-
-    sort = sorted(compared_images, key=lambda x: x[1])
-    return sort
 
 
 def image_comparison(my_image, image_objects):
@@ -43,7 +11,7 @@ def image_comparison(my_image, image_objects):
     :param image_objects: the photos from the photographer
     :param my_image: image to be compared
     :return :return a list of tuple structure
-    (photo_name,euclidean_difference)
+    (photo_name,euclidean_difference,photographer_id)
     """
     # open my image
     my_image = Image.open(my_image)
@@ -69,7 +37,7 @@ def image_comparison(my_image, image_objects):
         difference = euclidean_distances([image], [my_image])
         # add a tuple of the photo id and the euclidean difference
         compared_images.append(
-            (image_object["photo"], difference[0][0])
+            (image_object["photo"], difference[0][0], image["photographid"])
         )
     # return sorted  according to the euclidean difference
     return sorted(compared_images, key=lambda x: x[1])
